436cda1fcd104853c2ff7a5cb44e1e72a221f031
mcardosso <89229485+mcardosso@users.noreply.github.com>
2021-10-21 19:50:33 -0300

    Initial commit

---
+++ 1 aula/ex008.js
@@ -0,0 +1,5 @@
+var vel=58.9
+console.log(`a velocidade do seu carro é ${vel}Km/h`)
+if(vel>60) {
+    console.log('você foi multado')}//condição simples.
+console.log('dirija sempre usando cinto de segurança')
---
+++ 1 aula/ex009.js
@@ -0,0 +1,5 @@
+var pais = 'EUA'
+console.log(`vivendo no(a) ${pais}`)
+if(pais=='EUA'){
+    console.log('youre american')}else{
+        console.log ('Você é estrangeiro')}
